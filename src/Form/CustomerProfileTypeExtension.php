735823067444083f42c9d8dd4a82e2f0f0459cff
vmoreau <vmoreau@datasolution.fr>
2023-02-21 18:40:04 +0100

    hotfix

--- src/Form/CustomerProfileTypeExtension.php
+++ src/Form/CustomerProfileTypeExtension.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace App\Form\Extension;
+namespace App\Form;
 
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
